623e95b895d38eb9d332d1284431cd1054eee79c
eli72828 <elitesler1@gmail.com>
2026-06-04 16:46:56 +0300

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-URL = "הכנס_כאן_את_הלינק_של_נספרסו"
+URL = "https://www.nespresso.com/il/he/orders/accessories/original/travel-tumbler-bubble-gum"
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 CHAT_ID = os.environ.get("CHAT_ID")
 
